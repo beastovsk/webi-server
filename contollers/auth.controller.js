16054c02786defdc1ff7979210127122a597577f
Danil Kostenko <coctenco@bk.ru>
2024-04-30 00:23:22 +0400

    Db,register, login, emailConfirm done

--- contollers/auth.controller.js
+++ contollers/auth.controller.js
@@ -26,7 +26,9 @@ const authController = {
 					.status(400)
 					.json({ message: "Пользователь не найден" });
 			}
-
+			if (!user.rows[0].is_confirmed) {
+				return res.status(400).json({ message: "Аккаунт не подтвержден. Проверьте вашу почту для подтверждения регистрации." });
+			}
 			const checkPass = await bcrypt.compare(
 				password,
 				user.rows[0].password
@@ -51,89 +53,69 @@ const authController = {
 				`SELECT * FROM "user" WHERE email = $1`,
 				[email]
 			);
-
+	
 			if (existingUser && existingUser.rows.length > 0) {
 				return res
 					.status(400)
 					.json({ message: "Этот пользователь уже зарегистрирован" });
 			}
-
+	
 			const hash = await bcrypt.hash(password, 10);
-			const confirmToken = await v4();
-
+			const confirmToken = v4(); // Создание уникального кода подтверждения
+	
 			const newUser = await db.query(
-				`INSERT INTO "user" (email, password, is_valid, confirm_token) VALUES ($1, $2, $3, $4) RETURNING id, email`,
+				`INSERT INTO "user" (email, password, is_confirmed, confirm_token) VALUES ($1, $2, $3, $4) RETURNING id, email`,
 				[email, hash, false, confirmToken]
 			);
-
 			const [user] = newUser.rows;
-
-			const token = await generateToken({
-				id: user.id,
-				email: user.email,
-			});
-
 			const mailOptions = {
 				from: "coctencoflez@gmail.com",
 				to: email,
 				subject: "Подтверждение регистрации",
-				text: `Поздравляем с успешной регистрацией! Перейдите по ссылке, чтобы подтвердить свою учетную запись: http://localhost:3000/confirm/${confirmToken}`,
+				text: `Ваедите код для подтверждения своей почты: ${confirmToken}`,
 			};
-
+	
 			transporter.sendMail(mailOptions, (error, info) => {
 				if (error) {
+					console.error("Ошибка отправки электронной почты:", error);
 					res.status(500).json({
 						error: "Ошибка отправки электронной почты",
 					});
 				} else {
 					res.json({
-						token,
 						message:
 							"Подтверждение регистрации отправлено на вашу почту",
 					});
 				}
 			});
 		} catch (error) {
+			console.error("Ошибка регистрации:", error);
 			res.status(500).json({ error: "Ошибка регистрации" });
 		}
 	},
 	confirmEmail: async (req, res) => {
 		try {
-			const { confirmCode } = req.body;
-			const [_, token] = await req.headers.authorization.split(" ");
-			const { email } = await decodeToken({ token });
-
-			const currentUser = await db.query(
-				`SELECT * FROM "user" WHERE email = $1`,
-				[email]
+			const { confirmToken } = req.body;
+			const { email } = req.body; 
+	
+			const user = await db.query(
+				`SELECT * FROM "user" WHERE email = $1 AND confirm_token = $2`,
+				[email, confirmToken]
 			);
-
-			if (!currentUser.rows.length) {
+	
+			if (!user.rows.length) {
 				return res
 					.status(400)
-					.json({ message: "Пользователь не найден" });
+					.json({ message: "Неверный код подтверждения" });
 			}
-
-			const [user] = currentUser.rows;
-
-			if (user.is_valid) {
-				return res
-					.status(400)
-					.json({ message: "Почта уже подтверждена" });
-			}
-
-			if (confirmCode != user.confirm_token) {
-				return res.status(400).json({
-					message: "Неверный код подтверждения",
-				});
-			}
-
-			await db.query(`UPDATE "user" SET is_valid = $2 WHERE email = $1`, [
+	
+			await db.query(`UPDATE "user" SET is_confirmed = true WHERE email = $1`, [
 				email,
-				true,
 			]);
-			return res.json({ message: "Почта подтверждена" });
+	
+			res.json({ message: "Почта подтверждена" });
 		} catch (error) {
+			console.error("Ошибка подтверждения:", error);
 			res.status(500).json({ error: "Ошибка подтверждения" });
 		}
 	},
