a1374fbf3836b0aeeec04244bde588c4f2ab82a4
Данила Смит <coctenco@bk.ru>
2024-05-01 15:41:56 +0000

    Merged with dev/createserver

--- database.sql
+++ database.sql
@@ -12,7 +12,7 @@ CREATE TABLE "service" (
     userId INTEGER REFERENCES "user"(id),
     title VARCHAR(255),
     price FLOAT,
-    images TEXT,
+    images TEXT[],
     videoLink VARCHAR(255),
     description TEXT,
     previewLink VARCHAR(255),
@@ -20,4 +20,13 @@ CREATE TABLE "service" (
     isHighlighted BOOLEAN,
     isPremium BOOLEAN,
     isVisible BOOLEAN
+);
+
+CREATE TABLE "order" (
+    id SERIAL PRIMARY KEY,
+    service_id INTEGER REFERENCES "service"(id),
+    user_id INTEGER REFERENCES "user"(id),
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
 );
