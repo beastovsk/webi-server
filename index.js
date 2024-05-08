82f0b2def00cae3a7e26ba6cc48046da2b874b79
Artyom Narshinov <narshinovartyom@mail.ru>
2024-05-08 12:46:41 +0400

    https cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 	origin: [
 		"http://localhost:3000",
 		"http://127.0.0.1:3000",
-		"webi-agency.ru",
+		"https://webi-agency.ru",
 	],
 	credentials: true,
 	methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
